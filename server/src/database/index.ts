675722949ad773c8d7f9a987ba49df56fd7b62fe
Bz-Carter <lebz2016@gmail.com>
2021-07-12 14:38:46 +0000

    typeScript Générics and adding types to my collections

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const user = "root_house_click";
 const userPassword = "IkaD64kJCph91R16";
@@ -7,7 +8,7 @@ const cluster = "cluster0.fmixu";
 const url =
   `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true });
 
   const db = client.db("main");
